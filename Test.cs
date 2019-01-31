c52838383d0ae5c316a752731c124d4c4cc04145
Andrzej Szygula <andrzej.szygula@outlook.com>
2019-01-31 14:43:32 +0100

    coomit 1

---
+++ Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endrju testy
+{
+    class CSS
+    {
+        IWebDriver m_driver;
+
+        [Test]
+        public void Pierwszy ()
+        {
+            m_driver = new ChromeDriver("C:\\geckodriver");
+            m_driver.Url = "http://demo.guru99.com/test/guru99home/";
+            m_driver.Manage().Window.Maximize();
+
+
+            // Store locator values of email text box and sign up button				
+            IWebElement emailTextBox = m_driver.FindElement(By.XPath(".//*[@id='philadelphia-field-email']"));
+            IWebElement signUpButton = m_driver.FindElement(By.XPath(".//*[@id='philadelphia-field-submit']"));
+
+            emailTextBox.SendKeys("test123@gmail.com");
+            signUpButton.Click();
+
+        }
+    }
+}
